8ae2690edc3afa6fd608de5badbd468a93dcdf43
chanchal <chanchal14b@gmail.com>
2025-11-16 13:03:46 +0530

    Fix API base URL for production

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const { register, login, deleteAccount } = require('../controllers/authController');
 const authMiddleware = require('../middleware/authMiddleware'); 
 
+
 router.post('/register', register);
 
 router.post('/login', login);
